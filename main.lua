f16dfe880198f6120a62104744b3b26d53abc4ed
Ronald Sin <36214598+ronaldsin@users.noreply.github.com>
2019-04-06 15:42:25 -0400

    update main.lua

--- main.lua
+++ main.lua
@@ -4,9 +4,10 @@
 
 -- initialize game
 function love.load()
-	sizeScale = 1
+	sizeScale = 1 -- default 1
 
-	hitboxScale = 8
+	hitboxConst = 10
+	hitboxScale = hitboxConst / sizeScale -- based on sizeScale
 
 	displayHitbox = false
 
@@ -15,60 +16,88 @@ function love.load()
 	require ("bullet")
 	require ("hitReg")
 	require ("sound")
+	require ("monster")
+	require ("inputs")
+	require ("keybindings")
 
+	-- create player obj
+	p = Player("lmao")
+	e = Monster("not lmao")
+	-- set enemy start temp
+	e.x = 500
+	e.y = 500
+
+	-- initialize bullet array
+	bullets = {}
+
+	--timers
+	--hitmarker timer
 	hit = false
 	counter = 0
 	hitTimer = 0.15
 
-	background = love.graphics.newImage("bg.png")
+	--fire timer
+	fireTimer = 0.1
+	fire_ani = fireTimer
+
+	--gun cd timer
+	cd = p.gunCd
+
+	--pause
+	pause = false
+
+	background = love.graphics.newImage("resources/bg.png")
 
 	-- setting crosshair cursor
-	setCursor("Crosshair.png")
+	setCursor("resources/Crosshair.png")
 
-	-- create player obj
-	p = Player()
-	e = Player()
-	e.name = "not lmao"
-	-- set enemy start temp
-	e.x = 500
-	e.y = 500--
-	-- create sound object
-
-	-- initialize bullet array
-	bullets = {}
 end
 
 function love.update(dt)
-	-- update data for player
-	p.update(dt)
-	e.update(dt)
+	if not pause then
+		-- update data for player
+		p.update(dt)
+		e.update(dt)
 
-	if hit then
-		if counter > hitTimer then
-			setCursor("Crosshair.png")
-			hit = false
+		keyboardDown(dt)
+
+		if hit then
+			if counter > hitTimer then
+				setCursor("resources/Crosshair.png")
+				hit = false
+			else
+				counter = counter + dt
+			end
 		else
-			counter = counter + dt
-		end
-	else
-		counter = 0
-	end
-
-	-- update data for every bullet
-	for i, v in ipairs(bullets) do
-		v.update(dt)
-
-		-- remove bullet from array if out of range
-		if v.traveled >= v.range then
-			table.remove(bullets, i)
+			counter = 0
 		end
 
+		fire_ani = fire_ani + dt
+
+		cd = cd + dt
+
+		-- update data for every bullet
+		for i, v in ipairs(bullets) do
+			v.update(dt)
+
+			-- remove bullet from array if out of range
+			if v.traveled >= v.range then
+				table.remove(bullets, i)
+			end
+
+		end
 	end
 end
 
 -- draw elements onto screen
 function love.draw()
+
 	love.graphics.draw(background, 0, 0, 0, 0.9, 0.9)
+
+	if fire_ani <= fireTimer then
+		love.graphics.draw(p.gunfire, p.x, p.y, p.rotation, 1 * sizeScale, 1 * sizeScale, p.gunfire:getWidth() / 2, p.gunfire:getHeight() / 2)
+		love.graphics.draw(e.gunfire, e.x, e.y, e.rotation, 1 * sizeScale, 1 * sizeScale, e.gunfire:getWidth() / 2, e.gunfire:getHeight() / 2)
+	end
 
 	-- draw bullets to screen
 	for i, v in ipairs(bullets) do
@@ -78,15 +107,11 @@ function love.draw()
 	-- draw player
 	e.draw()
 	p.draw()
-end
 
--- press mouse 1 to shoot
-function love.mousepressed(x, y, button, isTouch)
-	if button == 1 then
-		gunshot:play()
-		b = proj(p.x, p.y, p.rotation, "lmao")
-		table.insert(bullets, b)
+	if pause then
+		love.graphics.print("Game is paused", 350, 280)
 	end
+
 end
 
 -- changes cursor to specified image 'file'
@@ -94,4 +119,11 @@ function setCursor(file)
 	Image = love.graphics.newImage(file)
 	cursor = love.mouse.newCursor(file, Image:getWidth() / 2, Image:getHeight() / 2)
 	love.mouse.setCursor(cursor)
+end
+
+function fire(x, y, r, n)
+	fire_ani = 0
+	playSound(gunshot)
+	b = proj(x, y, r, n)
+	table.insert(bullets, b)
 end
