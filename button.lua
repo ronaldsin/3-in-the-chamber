ad64a25d660bea26c7ed8a415f7739e4786e533d
Ronald <ronald.sin@uoit.net>
2019-05-04 19:38:22 -0400

    added comments to button

--- button.lua
+++ button.lua
@@ -7,7 +7,7 @@ buttonId = 0
 
 	demoButton.image = love.graphics.newImage("image")
 	demoButton.hoverImage = love.graphics.newImage("hoverImage")
-	
+
 	demoButton.hover = true
 	demoButton.update()
 ]]--
@@ -24,6 +24,7 @@ function createButton(x1, y1, x2, y2)
 
 	local button = {} -- create button table
 
+	-- check which Coordinate is left and which is top
 	if x1 < x2 then
 		button.left = x1
 		button.right = x2
@@ -42,11 +43,15 @@ function createButton(x1, y1, x2, y2)
 		button.bottom = y1
 	end
 
+	-- the width of the button
 	button.width = button.right - button.left
 	button.height = button.bottom - button.top
 
+	-- button properties
+
 	button.hover = false -- if the button changes on hover
-	--button.pressed = false
+	--button.pressed = false -- if the button changes when pressed
+	button.outline = false -- if the button draws its outline
 
 	button.visable = true -- if button is visable
 
@@ -61,37 +66,49 @@ function createButton(x1, y1, x2, y2)
 	button.currentImage = Nil -- current image of button
 	button.currentText = Nil -- current text of button
 
-	button.inBounds = Nil
+	button.inBounds = Nil -- if the mouse is in the button bounds
 
 	button.id = buttonId -- the button id for event calls
 
 	buttonId = buttonId + 1 -- ensures every button has a diffrent id
 
-	-- love.event.push("test")
-
+	-- draws the button based on the current image and text
 	function button.draw()
-		if not (button.currentImage == Nil) then
-			love.graphics.draw(button.currentImage, button.left, button.top)
-		end
+		-- only draw when button is visable
+		if button.visable then
 
-		if not (button.currentText == Nil) then
-			--centers text
-			love.graphics.print(button.currentText, button.left + button.width / 2, button.top + button.height / 2 )
-		end
+			-- draws the image if its not nil
+			if not (button.currentImage == Nil) then
+				love.graphics.draw(button.currentImage, button.left, button.top)
+			end
 
-		love.graphics.rectangle("line", button.left, button.top, button.width, button.height)
+			-- draws the text if not nil
+			if not (button.currentText == Nil) then
+				--centers text
+				love.graphics.print(button.currentText, button.left + button.width / 2, button.top + button.height / 2 )
+			end
+
+			-- draws the button outline if button outline is true
+			if button.outline then
+				love.graphics.rectangle("line", button.left, button.top, button.width, button.height)
+			end
+		end
 	end
 
+	-- updates the button images and text once (only call once after initilization)
 	function button.update()
-		button.currentImage = button.image -- current image of button
+		button.currentImage = button.image
 		button.currentText = button.text
 	end
 
+	-- checks if the mouse is in the button when the mouse moves
 	function love.mousemoved( x, y, dx, dy, istouch )
 		-- check if mouse is in button
-		if x >= button.left and x <= button.right and y >= button.top and y <= button.bottom then
+		if x >= button.left and x <= button.right and y >= button.top and y <= button.bottom then -- if mouse is in bounds
 			button.inBounds = true
-			if button.hover and button.visable then
+
+			-- if in bounds and the button hover is true than change current button to hover
+			if button.hover then
 				button.currentImage = button.hoverImage
 				button.currentText = button.hoverText
 			end
