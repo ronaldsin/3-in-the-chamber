f354bf5262fd5547f05958d39388702671a18dc9
Ronald <ronald.sin@uoit.net>
2019-05-04 05:16:05 -0400

    go to game when mouse is clicked in menu

--- inputs.lua
+++ inputs.lua
@@ -4,9 +4,13 @@ function love.mousepressed(x, y, button, isTouch, dt)
 
 	-- if mouse is pressed while in menu, start the game
 	if gameState == "menu" then
-		gameState = "game"
-		gameLoad()
-		love.graphics.setNewFont(40)
+
+		gameState = "game" -- change game state into actual game (starts game)
+
+		gameLoad() -- initalizes game -> game.lua
+
+		love.graphics.setNewFont(40) -- font size 40
+
 	end
 
 end
