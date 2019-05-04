522e376a6874b9c1fc102fa3f257983fd63c1842
Ronald <ronald.sin@uoit.net>
2019-05-04 05:16:55 -0400

    add menu animation

--- menu.lua
+++ menu.lua
@@ -1,10 +1,16 @@
 -- menuLoad() loads the initial menu state
 function menuLoad()
 
+	-- create new animation object -> main character portrait
+	menuImage = createAnimation("Ponytail_CSS", 4, 6, 2, 2, 1, 256, 256, true)
+
 end
 
 -- menuUpdate() updates menu state for changes
 function menuUpdate(dt)
+
+	-- update image animation frame
+	menuImage.update(dt)
 
 end
 
@@ -13,6 +19,9 @@ function menuDraw()
 
 	-- Menu information message
 	love.graphics.print("You are in menu left click to start", camera.width - 20, camera.height)
+
+	-- draws the menu image
+	menuImage.draw(camera.width - menuImage.frameWidth / 4, camera.height - menuImage.frameHeight / 10, 0)
 
 	-- image = love.graphics.newImage("imagelocation.png")
 	love.graphics.print("")
