20fc1f42420a25e1584886dfd759e7642039df46
Ronald <ronald.sin@uoit.net>
2019-05-04 05:15:50 -0400

    set up camera and player

--- game.lua
+++ game.lua
@@ -1,18 +1,26 @@
 -- gameLoad() initalizes the game state and variables
 function gameLoad()
 
+	-- load the background image
 	background = love.graphics.newImage("resources/Map_1_with_text_5000.png")
+
+	player = createPlayer()
 
 end
 
 -- gameUpdate(dt) updates game state real time
 function gameUpdate(dt)
 
+	camera.update(dt)
+
 end
 
 -- gameDraw() draws game state and changes onto screen
 function gameDraw()
 
+	camera.draw()
+
+	-- draw the background
 	love.graphics.draw(background)
 
 end
